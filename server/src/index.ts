b6adc2327c4aa50c165693c921543ddd56e479dd
Jinsu <wlstn8738@naver.com>
2022-06-24 22:29:08 +0900

    Add monorepo and init server

---
+++ server/src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+
+(async () => {
+  const server = new ApolloServer(null);
+
+  const app = express();
+  await server.start();
+  server.applyMiddleware({
+    app,
+    path: '/graphql',
+    cors: {
+      origin: ['https://localhost:3000'],
+      credentials: true,
+    },
+  })
+  await app.listen({ port: 8000 });
+  console.log('server listening on 8000 ...');
+})()
