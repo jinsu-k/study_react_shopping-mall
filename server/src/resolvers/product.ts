6ac920d384687beb1a8a0cd3ba7dab2d1c461a2a
Jinsu <wlstn8738@naver.com>
2022-06-25 16:49:58 +0900

    Update resolver

--- server/src/resolvers/product.ts
+++ server/src/resolvers/product.ts
@@ -1,22 +1,15 @@
-import { Resolver } from "./types";
+import { DBField } from "../dbController";
+import { Product, Resolver } from "./types";
 
-const mockProducts = (() => 
-  Array.from({ length: 20 }).map((_, i) => ({
-      id: i + 1 + '',
-      imageUrl: `https://picsum.photos/id/${i+10}/200/150/`,
-      price: 50000,
-      title: `임시상품${i+1}`,
-      description: `임시상세내용${i+1}`,
-      createAt: new Date(1655729878703+(i*1000*60*60*10)).toString()
-})))()
+
 
 const productResolver: Resolver = {
   Query: {
-    products: (parent, args, context, info) => {
-      return mockProducts;
+    products: (parent, args, { db }) => {
+      return db.products
     },
-    product: (parent, { id }, context, info) => {
-      const found = mockProducts.find(item => item.id === id);
+    product: (parent, { id }, { db }) => {
+      const found = db.products.find(item => item.id === id);
       if (found) return found;
       return null;
     },
--- server/src/resolvers/types.ts
+++ server/src/resolvers/types.ts
@@ -3,8 +3,31 @@ export type Resolver = {
     [key: string]: (
       parent: any,
       args: { [key: string]: any},
-      context: {},
+      context: {
+        db: {
+          cart: Cart
+          products: Products
+        }
+      },
       info: any
      ) => any
   }
 }
+
+export type Product = {
+  id: string
+  imageUrl: string
+  prcie: number
+  title: string
+  description: string
+  createAt: number
+}
+
+export type Products = Product[];
+
+export type CartItem = {
+  id: string
+  amount: number
+}
+
+export type Cart = CartItem[]
